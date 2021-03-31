0675f2b3bed81b30b62b6443ce8e5a6487409fa1
Manjul Bhattarai <manjul.atern@gmail.com>
2021-03-31 08:49:21 +0545

    Datatables and Django Pagination implementation

--- Elective2021One/studentsapp/urls.py
+++ Elective2021One/studentsapp/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index),
     path("search/", views.search,name="search"),
+    path("list/", views.listStudents,name="listStudents"),
+    path("paginate/", views.listStudentsPaginate,name="listStudentsPaginate"),
     ]
--- Elective2021One/studentsapp/views.py
+++ Elective2021One/studentsapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
+
 from .models import Student
 
 # Create your views here.
@@ -17,4 +19,25 @@ def search(request):
 	context = {"students": students}
 	template = loader.get_template('search_result.html')
 	return HttpResponse(template.render(context, request))
+
+def listStudents(request):
+	template = loader.get_template('student_list_datatable.html')
+	students = Student.objects.all()
+	return HttpResponse(template.render({'students':students}, request))
+
+def listStudentsPaginate(request):
+	template = loader.get_template('student_list_paginate.html')
+	student_list = Student.objects.all()
+
+	page = request.GET.get('page', 1)
+	paginator = Paginator(student_list,3)
+
+	try:
+		students =  paginator.page(page)
+	except PageNotAnInteger:
+		students =  paginator.page(1)
+	except EmptyPage:
+		students =  paginator.page(paginator.num_pages)
+
+	return HttpResponse(template.render({'students':students}, request))
 
